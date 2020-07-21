1e13641824e780c4e452af1b2b4f0c4efb468f0a
JolyneFr <934659050@qq.com>
2020-07-21 11:54:07 +0800

    struct ray

---
+++ src/ray.rs
@@ -0,0 +1,28 @@
+pub use crate::vec3::Point3;
+pub use crate::vec3::Vec3;
+
+pub struct Ray {
+    pub orig: Point3,
+    pub dir: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Self {
+        Self {
+            orig: Point3 {
+                x: origin.x,
+                y: origin.y,
+                z: origin.z,
+            },
+            dir: Vec3 {
+                x: direction.x,
+                y: direction.y,
+                z: direction.z,
+            },
+        }
+    }
+
+    pub fn at(self, t: f64) -> Point3 {
+        self.orig + self.dir * t
+    }
+}
